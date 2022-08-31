f6bdecdefe79943bff632d5f0ac493474914cc0d
hanfengx <1779833186@qq.com>
2022-08-31 15:00:06 +0800

    删除测试接口，新增登录和注册页面 @author hanfx

--- src/main.js
+++ src/main.js
@@ -7,9 +7,18 @@ import VueAxios from 'vue-axios'
 import ElementUI from 'element-plus'
 import 'element-plus/theme-chalk/index.css'
 
-
+//设置axios为form-data
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.headers.get['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.transformRequest = [function (data) {
+    let ret = ''
+    for (let it in data) {
+      ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+    }
+    return ret
+}]
 
 //让请求携带上浏览器的cookie
-axios.defaults.withCredentials=true
+axios.defaults.withCredentials=true 
 axios.defaults.baseURL ="http://localhost:8080" //封装默认路径
 createApp(App).use(router).use(store).use(ElementUI).use(VueAxios,axios).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,20 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginHome from '../views/LoginHome.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'login',
+    component: LoginHome
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/LoginRegistered.vue')
+  // }
 ]
 
 const router = createRouter({
